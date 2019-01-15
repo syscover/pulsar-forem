7bce3434861ce63878818d77bfff5c310dd1fc74
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2019-01-15 17:06:23 +0100

    add category relation

--- src/Syscover/Forem/Models/Group.php
+++ src/Syscover/Forem/Models/Group.php
@@ -16,6 +16,9 @@ class Group extends CoreModel
 
     protected $table        = 'forem_group';
     protected $fillable     = ['id', 'code', 'name', 'slug', 'category_id', 'target_id', 'assistance_id', 'type_id', 'hours', 'price', 'price_hour', 'contents', 'requirements', 'observations', 'action_id', 'expedient_id', 'employment_office_id', 'starts_at', 'ends_at', 'selection_date', 'open', 'schedule', 'publish', 'is_product', 'product_id', 'country_id', 'territorial_area_1_id', 'territorial_area_2_id', 'territorial_area_3_id', 'zip', 'locality', 'address', 'latitude', 'longitude'];
+    public $with            = [
+        'category'
+    ];
 
     /**
      * Is not possible add 'attachments' to $with parameter, it need to be instantiated to get lang parameter
@@ -35,6 +38,11 @@ class Group extends CoreModel
             'id'
         )
             ->orderBy('sort', 'asc');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     // Accessors
