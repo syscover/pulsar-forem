328c017326ca13d627135a0f94d34dd8f48a149b
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-11 14:06:48 +0100

    minor fix

--- src/Syscover/Forem/GraphQL/Services/ProvinceGraphQLService.php
+++ src/Syscover/Forem/GraphQL/Services/ProvinceGraphQLService.php
@@ -4,7 +4,6 @@ use Syscover\Core\GraphQL\Services\CoreGraphQLService;
 use Syscover\Forem\Models\Province;
 use Syscover\Forem\Services\CategoryService;
 
-
 class ProvinceGraphQLService extends CoreGraphQLService
 {
     public function __construct(Province $model, CategoryService $service)
