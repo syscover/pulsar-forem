5ce3830608788514a238fd53f4d8a086773ef915
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-05-26 21:36:05 +0200

    bulding

--- src/Syscover/Forem/GraphQL/Services/InscriptionGraphQLService.php
+++ src/Syscover/Forem/GraphQL/Services/InscriptionGraphQLService.php
@@ -3,6 +3,7 @@
 use Syscover\Core\GraphQL\Services\CoreGraphQLService;
 use Syscover\Forem\Models\Inscription;
 use Syscover\Forem\Services\InscriptionService;
+use Spatie\ArrayToXml\ArrayToXml;
 
 class InscriptionGraphQLService extends CoreGraphQLService
 {
@@ -10,5 +11,34 @@ class InscriptionGraphQLService extends CoreGraphQLService
     {
         $this->model = $model;
         $this->service = $service;
+    }
+
+    public function export($root, array $args)
+    {
+        $incriptions = Inscription::where('group_id', $args['id'])
+            ->where('exported', false)
+            ->get();
+
+
+        // group inscriptions each 10
+        $n = (int)($incriptions->count() / 10);
+        if (($incriptions->count() % 10) > 0) $n++;
+        $inscriptionsGroups = $incriptions->split($n);
+
+
+        foreach ($inscriptionsGroups as $inscriptionsGroup)
+        {
+            // crear XML
+        }
+
+
+
+
+        // spatie/array-to-xml
+        // create files
+        // create zip
+        // donwload zip
+
+        return [1, 2, 3];
     }
 }
