de9ff3459cb996bda38d0b3afa6ef07483aedbc2
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-05-29 23:07:50 +0200

    export inscriptions

--- src/Syscover/Forem/GraphQL/Services/InscriptionGraphQLService.php
+++ src/Syscover/Forem/GraphQL/Services/InscriptionGraphQLService.php
@@ -1,6 +1,9 @@
 <?php namespace Syscover\Forem\GraphQL\Services;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Syscover\Core\GraphQL\Services\CoreGraphQLService;
+use Syscover\Forem\Models\Group;
 use Syscover\Forem\Models\Inscription;
 use Syscover\Forem\Services\InscriptionService;
 use Spatie\ArrayToXml\ArrayToXml;
@@ -15,30 +18,64 @@ class InscriptionGraphQLService extends CoreGraphQLService
 
     public function export($root, array $args)
     {
-        $incriptions = Inscription::where('group_id', $args['id'])
+        $group          = Group::find($args['id']);
+        $incriptions    = Inscription::where('group_id', $args['id'])
             ->where('exported', false)
             ->get();
-
 
         // group inscriptions each 10
         $n = (int)($incriptions->count() / 10);
         if (($incriptions->count() % 10) > 0) $n++;
-        $inscriptionsGroups = $incriptions->split($n);
+        $inscriptionGroups = $incriptions->split($n);
 
-
-        foreach ($inscriptionsGroups as $inscriptionsGroup)
+        $files = [];
+        foreach ($inscriptionGroups as $inscriptionGroup)
         {
-            // crear XML
+            $export = [
+                'lista_solicitudes_inscripcion' => []
+            ];
+            foreach ($inscriptionGroup as $inscription)
+            {
+                $export['lista_solicitudes_inscripcion'][] = [
+                    'solicitud_inscripcion' => [
+                        'expediente_grupo'      => $inscription->code,
+                        'tiene_registro_clm'    => $inscription->has_registry ? 1 : 0,
+                    ]
+                ];
+            }
+
+            $xml = ArrayToXml::convert($export, ['rootElementName' => 'documento']);
+
+            $filename = $group->id . '--' . Str::uuid() . '.xml';
+            $files[] = $filename;
+            Storage::disk('local')->put('public/forem/export/' . $filename, $xml);
         }
 
 
+        // Create zip file
+        $zipFile = $group->id . '--' . Str::uuid() . '.zip';
+        $pathZipFile = storage_path('app/public/forem/export/' . $zipFile);
 
+        $zip = new \ZipArchive();
 
-        // spatie/array-to-xml
-        // create files
-        // create zip
-        // donwload zip
+        if ($zip->open($pathZipFile, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) === true)
+        {
+            foreach ($files as $file)
+            {
+                if (file_exists(storage_path('app/public/forem/export/' . $file)))
+                {
+                    info(storage_path('app/public/forem/export/' . $file));
+                    $zip->addFile(storage_path('app/public/forem/export/' . $file), $file);
+                }
 
-        return [1, 2, 3];
+            }
+            $zip->close();
+        }
+
+        // set like exported
+
+        // delete xml
+
+        return $zipFile;
     }
 }
