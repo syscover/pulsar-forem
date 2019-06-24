8d6866c1f332441c478fe82520b6f29fbbdef6ac
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.com>
2019-06-24 21:28:10 +0200

    minor fix

--- src/database/seeds/ForemResourceSeeder.php
+++ src/database/seeds/ForemResourceSeeder.php
@@ -8,18 +8,19 @@ class ForemResourceSeeder extends Seeder
     public function run()
     {
         Resource::insert([
-            ['id' => 'forem',                   'name' => 'Forem Package',      'package_id' => 500],
-            ['id' => 'forem-category',          'name' => 'Categories',         'package_id' => 500],
-            ['id' => 'forem-employment-office', 'name' => 'Employment offices', 'package_id' => 500],
-            ['id' => 'forem-expedient',         'name' => 'Expedients',         'package_id' => 500],
-            ['id' => 'forem-action',            'name' => 'Actions',            'package_id' => 500],
-            ['id' => 'forem-group',             'name' => 'Groups',             'package_id' => 500],
-            ['id' => 'forem-group-office',      'name' => 'Groups',             'package_id' => 500],
-            ['id' => 'forem-student',           'name' => 'Students',           'package_id' => 500],
-            ['id' => 'forem-inscription',       'name' => 'Inscriptions',       'package_id' => 500],
-            ['id' => 'forem-database',          'name' => 'Database',           'package_id' => 500],
-            ['id' => 'forem-province',          'name' => 'Provinces',          'package_id' => 500],
-            ['id' => 'forem-locality',          'name' => 'Localities',         'package_id' => 500],
+            ['id' => 'forem',                       'name' => 'Forem Package',      'package_id' => 500],
+            ['id' => 'forem-category',              'name' => 'Categories',         'package_id' => 500],
+            ['id' => 'forem-employment-office',     'name' => 'Employment offices', 'package_id' => 500],
+            ['id' => 'forem-expedient',             'name' => 'Expedients',         'package_id' => 500],
+            ['id' => 'forem-action',                'name' => 'Actions',            'package_id' => 500],
+            ['id' => 'forem-group',                 'name' => 'Groups',             'package_id' => 500],
+            ['id' => 'forem-group-office',          'name' => 'Groups',             'package_id' => 500],
+            ['id' => 'forem-student',               'name' => 'Students',           'package_id' => 500],
+            ['id' => 'forem-inscription',           'name' => 'Inscriptions',       'package_id' => 500],
+            ['id' => 'forem-inscription-office',    'name' => 'Inscriptions',       'package_id' => 500],
+            ['id' => 'forem-database',              'name' => 'Database',           'package_id' => 500],
+            ['id' => 'forem-province',              'name' => 'Provinces',          'package_id' => 500],
+            ['id' => 'forem-locality',              'name' => 'Localities',         'package_id' => 500],
         ]);
     }
 }
