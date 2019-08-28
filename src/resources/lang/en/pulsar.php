32108b1fde7861be6ac3c77d0a074984039fa6a4
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2019-08-28 22:23:58 +0200

    translations

--- src/resources/lang/en/pulsar.php
+++ src/resources/lang/en/pulsar.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'unemployed_situations'                                 => 'Unemployed situations'
+    'academic_levels'                                       => 'Academic levels',
+    'action'                                                => 'Action',
+    'availabilities'                                        => 'Availabilities',
+    'employment_office'                                     => 'Employment office',
+    'expedient'                                             => 'Expedient',
+    'group'                                                 => 'Group',
+    'province'                                              => 'Province',
+    'unemployed_situations'                                 => 'Unemployed situations',
 ];
--- src/resources/lang/es/pulsar.php
+++ src/resources/lang/es/pulsar.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'unemployed_situations'                             => 'Situación de desempleo'
+    'academic_levels'                                       => 'Niveles académicos',
+    'action'                                                => 'Acción',
+    'availabilities'                                        => 'Disponibilidad',
+    'employment_office'                                     => 'Oficina de empleo',
+    'expedient'                                             => 'Expediente',
+    'group'                                                 => 'Grupo',
+    'province'                                              => 'Provincia',
+    'unemployed_situations'                                 => 'Situación de desempleo',
 ];
